05b0af338d490672fb3a316541466f9cb295fbed
William Hobson <will.hobson85@gmail.com>
2022-12-07 17:10:01 -0500

    Implement Router

--- src/Components/TherapistCard/TherapistCard.js
+++ src/Components/TherapistCard/TherapistCard.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./TherapistCard.css";
 
 const TherapistCard = ({ id, name, labels, imageUrl }) => {
 
     return (
-      <div className="therapist-card">
+      <Link to={`/${id}`} className="therapist-card">
         <img src={imageUrl} alt="therapist profile"/>
         <p>Name: {name} </p>
         <p>Address: </p>
         <p>Contact: </p>
         <p>Tags: {labels} </p>
-      </div>
+      </Link>
     )
 }
 
