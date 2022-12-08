f0878ce4070c147582344dbcac577283f7099170
Emily Miles <ms.emily.j.miles@gmail.com>
2022-12-08 09:55:08 -0500

    Update uri to match new mock server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
-  uri: 'https://3af302d9-8dd1-4bfc-8829-fdca59536d4d.mock.pstmn.io',
+  uri: 'https://b4e60499-e3a0-4c58-b2c4-78c2f78fc554.mock.pstmn.io/graphql',
   cache: new InMemoryCache()
-})
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
+    <ApolloProvider client={ client }>
       <App />
     </ApolloProvider>
   </BrowserRouter>
