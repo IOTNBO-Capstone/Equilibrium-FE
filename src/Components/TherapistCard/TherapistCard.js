23eff5a0034b67a4491b19443368a1dbbe39d4ce
Emily Miles <ms.emily.j.miles@gmail.com>
2022-12-10 15:58:52 -0500

    Fix key attribute error that showed up when filtering

--- src/Components/TherapistCard/TherapistCard.js
+++ src/Components/TherapistCard/TherapistCard.js
@@ -8,7 +8,8 @@ const TherapistCard = ({ id, name, labels, imageUrl, address, phoneNumber, pract
   const cityState = splitAdress.slice(1, 3).join(', ');
 
   const listLabels = JSON.parse(labels);
-  const formatLabels = listLabels.map((label, index) => <li key={ index } className="one-label">{ label }</li>);
+  console.log(listLabels);
+  const formatLabels = listLabels.map((label, index) => <li key={ `${label}${index}` } className="one-label">{ label }</li>);
 
   // phoneNumber needs to be formated for consistency
   // currently coming back as (111)111-1111 or 111-111-1111 or 111.111.1111
