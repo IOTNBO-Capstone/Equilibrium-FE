a049db5e71318c15ba74d8b7cb02cf357d695473
Emily Miles <ms.emily.j.miles@gmail.com>
2022-12-09 22:13:04 -0500

    Add filter functionailty for tags

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTherapists } from '../../utilities';
 import TherapistCard from '../TherapistCard/TherapistCard';
 import "./LandingPage.css";
 
 const LandingPage = () => {
+  const [filteredTags, setFilteredTags] = useState([]);
+  const [filteredTherapists, setFilteredTherapists] = useState([]);
+
   const { data, error, loading } = useTherapists();
 
-  if (loading && !data) return "Loading...";
+  const getFilterTags = data.therapists.reduce((list, therapist) => {
+    const formatLabels = JSON.parse(therapist.labels);
 
-  if (error) return `${error.message}`;
+    formatLabels.forEach(label => {
+      if (!list.includes(label)) {
+        list.push(label);
+      }
+    });
 
-  const cards = data.therapists.map(therapist => {
+    return list;
+  }, []);
+
+  const getCheckedTags = (checked) => {
+    if (!filteredTags.includes(checked)) {
+      setFilteredTags([...filteredTags, checked]);
+    } else {
+      setFilteredTags(filteredTags.filter(tag => tag !== checked));
+    }
+  };
+
+  const searchTags = getFilterTags.map((tag, index) => {
+    return (
+      <form key={ index } className="single-tag">
+        <input
+          type="checkbox"
+          id={ `tag${index}` }
+          name={ `tag${index}` }
+          value={ tag }
+          checked={ filteredTags.includes(tag) ? true : false }
+          disabled={ filteredTags.length === 5 && !filteredTags.includes(tag) ? true : false }
+          onChange={ (event) => getCheckedTags(event.target.value) }
+        />
+        <label>{ tag }</label>
+      </form>
+    );
+  });
+
+  const allCards = data.therapists.map(therapist => {
     return (
       <TherapistCard
         id={ therapist.id }
@@ -25,34 +61,17 @@ const LandingPage = () => {
     );
   });
 
-  const getFilterTags = data.therapists.reduce((list, therapist) => {
-    const formatLabels = JSON.parse(therapist.labels);
-
-    formatLabels.forEach(label => {
-      if (!list.includes(label)) {
-        list.push(label);
-      }
-    });
-
-    return list;
-  }, []);
-
-  const searchTags = getFilterTags.map((tag, index) => {
-    return (
-      <form key={ index } className="single-tag">
-        <input type="checkbox" id={ `tag${index}` } name={ `tag${index}` } value={ tag } />
-        <label>{ tag }</label>
-      </form>
-    );
-  });
-
+  console.log('therapist', filteredTherapists);
+  console.log('tags', filteredTags);
   return (
     <section className="landing-page-container">
       <div className="container search-tags-container">
         { searchTags }
       </div>
       <div className="container therapist-cards-container">
-        { cards }
+        { (loading && !data) && <h1>Loading...</h1> }
+        { error && <h1>There was an error loading the data. { `${error.message}` }</h1> }
+        { allCards }
       </div>
     </section>
   );
