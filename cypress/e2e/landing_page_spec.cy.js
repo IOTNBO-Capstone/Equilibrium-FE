462b43603d8cf50af22677845e3d0b33e66c9c07
Emily Miles <ms.emily.j.miles@gmail.com>
2022-12-13 12:03:04 -0500

    Refactor tests to match changes for accessibility

--- cypress/e2e/landing_page_spec.cy.js
+++ cypress/e2e/landing_page_spec.cy.js
@@ -1,134 +1,134 @@
 describe('Landing Page Flows', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/');
   });
 
   it('Should be able to see the title', () => {
     cy.get('[data-cy="title"]')
       .should('be.visible')
-      .contains('Equilibrium')
+      .contains('Equilibrium');
   });
 
   it('Should be able to see a display of therapists', () => {
     cy.get('[data-cy="landing-page"]')
-      .should('be.visible')
+      .should('be.visible');
   });
 
   it('Should display optional labels to filter by', () => {
     cy.get('[data-cy="search-tags"]')
-    .children()
-    .should('be.visible')
-    .should('have.length', 13)
+      .children()
+      .should('be.visible')
+      .should('have.length', 13);
     cy.get('[data-cy="label-12"]')
       .should('exist')
-      .contains('POC')
-      cy.get('[data-cy="label-13"]')
-      .should('not.exist')
+      .contains('POC');
+    cy.get('[data-cy="label-13"]')
+      .should('not.exist');
   });
 
   it('Should have a container with the therapists', () => {
     cy.get('[data-cy="therapists-card-container"]')
       .children()
       .should('exist')
-      .should('have.length', 10)
+      .should('have.length', 10);
     cy.get('[href="/10"]')
-      .should('exist')
+      .should('exist');
     cy.get('[href="/11"]')
-      .should('not.exist')
+      .should('not.exist');
   });
 
   it('Should have a therapist\'s information', () => {
     cy.get('[href="/1"]')
-      .should('exist')
+      .should('exist');
     cy.get('[href="/1"] > img')
-      .should('exist')
-      // .contains('https://loremflickr.com/300/300')
+      .should('exist');
+    // .contains('https://loremflickr.com/300/300')
     cy.get('[href="/1"] > h2')
-      .should('exist')
-      // .contains('Sarah Jones')
+      .should('exist');
+    // .contains('Sarah Jones')
     cy.get('[href="/1"] > :nth-child(3)')
       .should('exist')
-      .contains('ABC therapy')
+      .contains('ABC therapy');
     cy.get('[href="/1"] > :nth-child(5)')
       .should('exist')
-      .contains('60136 Heidenreich Via')
+      .contains('60136 Heidenreich Via');
     cy.get('[href="/1"] > :nth-child(6)')
       .should('exist')
-      .contains('New Meta, OR 03728-2318')
+      .contains('New Meta, OR 03728-2318');
     cy.get('[href="/1"] > :nth-child(8)')
       .should('exist')
-      .contains('766.314.3980')
-    cy.get('[href="/1"] > :nth-child(10)')
+      .contains('766.314.3980');
+    cy.get('[href="/1"] > ul > :nth-child(1)')
+      .should('exist');
+    // .contains('Works with')
+    cy.get('[href="/1"] > ul > :nth-child(2)')
       .should('exist')
-      // .contains('Works with')
-    cy.get('[href="/1"] > :nth-child(11)')
+      .contains('In-house Pharmacy');
+    cy.get('[href="/1"] > ul > :nth-child(3)')
       .should('exist')
-      .contains('In-house Pharmacy')
-    cy.get('[href="/1"] > :nth-child(12)')
+      .contains('Works with Trauma-based disorders');
+    cy.get('[href="/1"] > ul > :nth-child(4)')
       .should('exist')
-      .contains('Works with Trauma-based disorders')
-    cy.get('[href="/1"] > :nth-child(13)')
-      .should('exist')
-      .contains('Grief Counseling')
+      .contains('Grief Counseling');
   });
 
   it('Should have a footer with links to external resources', () => {
     cy.get('[data-cy="resources"]')
-      .should('exist')
+      .should('exist');
 
     cy.get('[data-cy="general-resources"]')
-      .should('exist')
+      .should('exist');
     cy.get('[data-cy="general-resources"] > h3')
       .should('exist')
-      .contains('General Mental Health')
+      .contains('General Mental Health');
     cy.get('[data-cy="general-resources"] > .webLink')
-      .should('exist')
-    
+      .should('exist');
+
     cy.get('[data-cy="coping-resources"]')
-      .should('exist')
+      .should('exist');
     cy.get('[data-cy="coping-resources"] > h3')
       .should('exist')
-      .contains('Coping Skills')
+      .contains('Coping Skills');
     cy.get('[href="https://success.oregonstate.edu/sites/success.oregonstate.edu/files/LearningCorner/Tools/stress_management_strategies.pdf"]')
       .should('exist')
-      .contains('Stress Management')
+      .contains('Stress Management');
     cy.get('[href="https://www.health.harvard.edu/blog/strategies-to-promote-better-sleep-in-these-uncertain-times-2020032719333"]')
       .should('exist')
-      .contains('Better Sleep')
+      .contains('Better Sleep');
     cy.get('[href="https://www.restorecenterla.com/10-coping-skills-for-addiction-recovery/"]')
-      .should('exist')  
-      .contains('Addiction Recovery')
+      .should('exist')
+      .contains('Addiction Recovery');
     cy.get('[href="https://www.threeoakshospice.com/blog/bereavement-strategies-coping-with-grief-loss/"]')
       .should('exist')
-      .contains('Bereavement Strategies')
+      .contains('Bereavement Strategies');
     cy.get('[href="https://ccofmooresville.com/101-ways-to-cope-with-anxiety-and-depression/"]')
       .should('exist')
-      .contains('Anxiety and Depression')
-    
+      .contains('Anxiety and Depression');
+
     cy.get('[data-cy="help-lines-resources"]')
-      .should('exist')
+      .should('exist');
     cy.get('[data-cy="help-lines-resources"] > h3')
       .should('exist')
-      .contains('Help Lines')
+      .contains('Help Lines');
     cy.get('[data-cy="help-lines-resources"] > .webLink')
-      .should('exist')
+      .should('exist');
   });
 
   it('Should be able to filter by tag', () => {
     cy.get('[data-cy="search-tags"]')
       .children()
       .should('be.visible')
-      .should('have.length', 13)
-    cy.get('#tag10').click()
+      .should('have.length', 13);
+    cy.get('#tag10').click();
     cy.get('[data-cy="therapists-card-container"]')
       .children()
       .should('be.visible')
-      .should('have.length', 5)
+      .should('have.length', 5);
   });
 
   it('Should be able to click a therapist\'s card and go to their individual page', () => {
-    cy.get('[href="/1"]').click()
-    cy.url().should('eq', 'http://localhost:3000/1')
+    cy.get('[href="/1"]').click();
+    cy.url().should('eq', 'http://localhost:3000/1');
   });
 });
