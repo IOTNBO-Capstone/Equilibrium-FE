b14dc97ef85cf61f9ec822d03ef82e66a8d1cfa6
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-12-13 20:52:27 -0600

    Add intercept to individual therapist spec

--- cypress/e2e/individual_therapist_spec.cy.js
+++ cypress/e2e/individual_therapist_spec.cy.js
@@ -1,6 +1,9 @@
 describe('Individual Therapist Flows', () => {
 
   beforeEach(() => {
+    cy.intercept("POST", "https://equilibrium.herokuapp.com/graphql", {
+      fixture: "data1.json"
+    })
     cy.visit('http://localhost:3000/1')
   });
 
